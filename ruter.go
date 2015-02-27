054e459068ef981265ad027fb99639fae39b364d
Michael Enger <michael@dekode.no>
2015-02-27 12:43:09 +0100

    Separate data fetching and data parsing to two separate functions
    Makes it easier to test.

--- ruter.go
+++ ruter.go
@@ -34,8 +34,6 @@ type sanntidArrivalData struct {
 // RequestArrivalData retrieves information about the upcoming arrivals for
 // a given location based on its locationId.
 func requestArrivalData(locationID int) ([]sanntidArrivalData, error) {
-	var data []sanntidArrivalData
-
 	url := fmt.Sprintf("http://reisapi.ruter.no/stopvisit/getdepartures/%d", locationID)
 
 	resp, err := http.Get(url)
@@ -49,7 +47,13 @@ func requestArrivalData(locationID int) ([]sanntidArrivalData, error) {
 		return nil, err
 	}
 
-	json.Unmarshal(body, &data)
+	return parseArrivalData(body), err
+}
 
-	return data, err
+func parseArrivalData(content []byte) []sanntidArrivalData {
+	var data []sanntidArrivalData
+
+	json.Unmarshal(content, &data)
+
+	return data
 }
